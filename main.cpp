a326f4c753aa90fbb6dd627c89371fe819508432
dunhamc13 <57208208+dunhamc13@users.noreply.github.com>
2020-06-17 10:31:24 -0700

    fixed after testing

--- main.cpp
+++ main.cpp
@@ -5,6 +5,9 @@
  * @Assumption: each input has exactly one soution and you may not use same element twice.
  * @Efficiency: Time = O(n^2), Space = O(1).
  */
+#include <stdexcept>
+#include <iostream>
+using namespace std;
 
 /* twoSum
  * @Description
@@ -15,20 +18,21 @@
  * @Param: nums, array of integers
  * @Param: target, integer to search for sum of two elements
 */
-void twoSum(int nums[], int target) {
-    //get size of array
-    int lengthNums = sizeof(nums)/sizeof(nums[0]);
-    //brute force check each element
-    for (int i = 0; i < lengthNums; i++){
-       for (int j = i + 1; j < lengthNums; j++){
-          if (j == target - i){
-            printf("Solution found at %d and %d", i,j);
-            return;
-          }//end if match
-       }//end for j
-    }//end for i
-    printf("No Solution");
-}//end two sum
+vector<int> twoSum(vector<int>& nums, int target) {
+//get size of array
+int lengthNums = sizeof(nums)/sizeof(nums[0]);
+//brute force check each element
+for (int i = 0; i < lengthNums; i++){
+   for (int j = i + 1; j < lengthNums; j++){
+      if (nums[j] == target - nums[i]){
+        printf("Solution found at %d and %d", i,j);
+        vector<int> solution{ i , j };
+        return solution;
+      }//end if match
+   }//end for j
+}//end for i
+throw std::invalid_argument("No Solution for Target");
+}
 
 
 /* main driver
